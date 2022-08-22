bb30e280e13bf71e0b2edb67bef6feb10f90b3d0
Konstantin Voykov <koykov0@gmail.com>
2022-08-22 18:26:30 +0300

    New config fields.

--- config.go
+++ config.go
@@ -2,12 +2,17 @@ package dlqdump
 
 import "time"
 
+const (
+	defaultFileMask = "{key}__%Y-%m-%d"
+)
+
 type Config struct {
-	Version     uint32
-	Key         string
-	Size        MemorySize
-	TimeLimit   time.Duration
-	Encoder     Encoder
-	Decoder     Decoder
-	Destination string
+	Version   uint32
+	Key       string
+	Size      MemorySize
+	TimeLimit time.Duration
+	Encoder   Encoder
+	Decoder   Decoder
+	Directory string
+	FileMask  string
 }
