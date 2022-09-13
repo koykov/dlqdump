e90e3b8e3b5e0d3c5c144924340784c918409b22
Konstantin Voykov <koykov0@gmail.com>
2022-09-14 00:02:56 +0300

    FS dumper: config draft.

--- io/fs.go
+++ io/fs.go
@@ -1,8 +1,21 @@
 package io
 
-import "github.com/koykov/blqueue"
+import (
+	"github.com/koykov/blqueue"
+	"github.com/koykov/dlqdump"
+)
 
-type FS struct{}
+type FS struct {
+	// Max buffer size in bytes.
+	// Dumper will move buffered data to destination file on overflow.
+	Buffer dlqdump.MemorySize
+	// Destination directory for dump files.
+	Directory string
+	// Dump file mask.
+	// Supports strftime patterns (see https://github.com/koykov/clock#format).
+	// If this param omit defaultFileMask ("%Y-%m-%d--%H-%M-%S--%i.bin") will use instead.
+	FileMask string
+}
 
 func (d FS) Dump(p []byte) (n int, err error) {
 	_ = p
