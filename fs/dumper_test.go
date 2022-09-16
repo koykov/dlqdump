4ed6e67eb9820018aa08e572998788bcb36f4608
Konstantin Voykov <koykov0@gmail.com>
2022-09-16 22:13:43 +0300

    Flush issue: cover with test.

--- fs/dumper_test.go
+++ fs/dumper_test.go
@@ -2,6 +2,7 @@ package fs
 
 import (
 	"testing"
+	"time"
 
 	"github.com/koykov/dlqdump"
 	"github.com/koykov/dlqdump/encoder"
@@ -51,6 +52,31 @@ func TestDumper(t *testing.T) {
 				t.Fatal(err)
 			}
 		}
+		if err = q.Close(); err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("timer", func(t *testing.T) {
+		q, err := dlqdump.New(&dlqdump.Config{
+			Version:       0,
+			Key:           "stage0",
+			Capacity:      dlqdump.Byte * 512,
+			FlushInterval: time.Millisecond * 10,
+			Encoder:       encoder.Basic{},
+			Dumper: &Dumper{
+				Directory: "testdata",
+				FileMask:  "timer--%Y-%m-%d--%H-%M-%S--%N.bin",
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		for i := 0; i < len(testVars); i++ {
+			if err = q.Enqueue(testVars[i]); err != nil {
+				t.Fatal(err)
+			}
+		}
+		time.Sleep(time.Millisecond * 20)
 		if err = q.Close(); err != nil {
 			t.Fatal(err)
 		}
