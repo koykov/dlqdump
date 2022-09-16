71cd987ad511370b309a5233c78ae6a38cdb7035
Konstantin Voykov <koykov0@gmail.com>
2022-09-16 21:48:26 +0300

    Cover reach size test.

--- fs/dumper_test.go
+++ fs/dumper_test.go
@@ -32,4 +32,27 @@ func TestDumper(t *testing.T) {
 			t.Fatal(err)
 		}
 	})
+	t.Run("size", func(t *testing.T) {
+		q, err := dlqdump.New(&dlqdump.Config{
+			Version:  0,
+			Key:      "stage0",
+			Capacity: dlqdump.Byte * 32,
+			Encoder:  encoder.Basic{},
+			Dumper: &Dumper{
+				Directory: "testdata",
+				FileMask:  "size--%Y-%m-%d--%H-%M-%S--%N.bin",
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		for i := 0; i < len(testVars); i++ {
+			if err = q.Enqueue(testVars[i]); err != nil {
+				t.Fatal(err)
+			}
+		}
+		if err = q.Close(); err != nil {
+			t.Fatal(err)
+		}
+	})
 }
