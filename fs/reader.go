6d1da7b901e5f2dd162fdb0988e8a62bd5430061
Konstantin Voykov <koykov0@gmail.com>
2022-10-02 23:33:19 +0300

    FS reader draft: init stage; read version from file header.

--- fs/reader.go
+++ fs/reader.go
@@ -1,13 +1,50 @@
 package fs
 
-import "github.com/koykov/dlqdump"
+import (
+	"encoding/binary"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/koykov/bytealg"
+	"github.com/koykov/dlqdump"
+)
 
 type Reader struct {
 	// MatchMask represents pattern to match the names of dump files.
 	// Mandatory param.
 	MatchMask string
+
+	once sync.Once
+	mux  sync.Mutex
+	mask string
+	f    *os.File
+	ver  dlqdump.Version
+	buf  []byte
 }
 
-func (r Reader) Read(dst []byte) (dlqdump.Version, []byte, error) {
-	return 0, dst, nil
+func (r *Reader) Read(dst []byte) (dlqdump.Version, []byte, error) {
+	r.once.Do(r.init)
+
+	r.mux.Lock()
+	defer r.mux.Unlock()
+	r.buf = r.buf[:0]
+
+	var err error
+	if r.f == nil {
+		if r.f, err = os.OpenFile("", os.O_RDONLY, 0644); err != nil {
+			return 0, dst, err
+		}
+		r.buf = bytealg.Grow(r.buf, 8)
+		if _, err = io.ReadAtLeast(r.f, r.buf, 8); err != nil {
+			return 0, dst, err
+		}
+		r.ver = dlqdump.Version(binary.LittleEndian.Uint64(r.buf))
+	}
+
+	return r.ver, dst, nil
+}
+
+func (r *Reader) init() {
+	r.mask = r.MatchMask
 }
