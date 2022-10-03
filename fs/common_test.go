5f812530d2a7cb4fc9995a3af37eb126f19d241c
Konstantin Voykov <koykov0@gmail.com>
2022-10-03 23:19:02 +0300

    Restorer testing issue: implement test queue.

--- fs/common_test.go
+++ fs/common_test.go
@@ -2,6 +2,7 @@ package fs
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 )
 
@@ -20,13 +21,49 @@ func (m m8r) MarshalTo(p []byte) (int, error) {
 
 type testq struct {
 	t *testing.T
+	c int
 }
 
-func (q testq) Enqueue(x interface{}) error { return nil }
-func (q testq) Size() int                   { return 0 }
-func (q testq) Capacity() int               { return 0 }
-func (q testq) Rate() float32               { return 0 }
-func (q testq) Close() error                { return nil }
+func (q *testq) Enqueue(x interface{}) error {
+	raw, ok := x.([]byte)
+	if !ok {
+		return errors.New("type mismatch")
+	}
+	s := string(raw)
+	switch q.c {
+	case 0:
+		if !bytes.Equal(raw, testVars[q.c].([]byte)) {
+			q.t.Fatalf("index %d: value mismatch", q.c)
+		}
+	case 1:
+		if !bytes.Equal(raw, *testVars[q.c].(*[]byte)) {
+			q.t.Fatalf("index %d: value mismatch", q.c)
+		}
+	case 2:
+		if s != testVars[q.c].(string) {
+			q.t.Fatalf("index %d: value mismatch", q.c)
+		}
+	case 3:
+		if s != *testVars[q.c].(*string) {
+			q.t.Fatalf("index %d: value mismatch", q.c)
+		}
+	case 4:
+		if s != testVars[q.c].(*bytes.Buffer).String() {
+			q.t.Fatalf("index %d: value mismatch", q.c)
+		}
+	case 5:
+		if !bytes.Equal(raw, testVars[q.c].(m8r).payload) {
+			q.t.Fatalf("index %d: value mismatch", q.c)
+		}
+	}
+	q.c++
+	return nil
+}
+func (q testq) Size() int     { return 0 }
+func (q testq) Capacity() int { return 0 }
+func (q testq) Rate() float32 { return 0 }
+func (q testq) Close() error  { return nil }
+func (q *testq) reset()       { q.t, q.c = nil, 0 }
 
 var testVars []interface{}
 
--- fs/reader_test.go
+++ fs/reader_test.go
@@ -2,6 +2,7 @@ package fs
 
 import (
 	"testing"
+	"time"
 
 	"github.com/koykov/dlqdump"
 	"github.com/koykov/dlqdump/decoder"
@@ -9,13 +10,14 @@ import (
 
 func TestReader(t *testing.T) {
 	conf := dlqdump.Config{
-		Version: dlqdump.ParseVersion("1.0"),
-		Key:     "example",
+		Version:       dlqdump.ParseVersion("1.0"),
+		Key:           "example",
+		CheckInterval: time.Hour, // will never happen during testing
 		Reader: &Reader{
 			MatchMask: "testdata/*.bin",
 		},
 		Decoder: decoder.Fallthrough{},
-		Queue:   testq{t: t},
+		Queue:   &testq{t: t},
 	}
 	rst, err := dlqdump.NewRestorer(&conf)
 	if err != nil {
@@ -25,4 +27,5 @@ func TestReader(t *testing.T) {
 	if err = rst.Restore(); err != nil {
 		t.Fatal(err)
 	}
+	_ = rst.ForceClose()
 }
