1793221f533db03edba341e8e0b514aba5c51470
Konstantin Voykov <koykov0@gmail.com>
2022-10-07 23:04:16 +0300

    Comment issues: queue.

--- queue.go
+++ queue.go
@@ -15,17 +15,23 @@ const (
 // Queue represents dumping queue.
 type Queue struct {
 	bitset.Bitset
+	// Config instance.
 	config *Config
+	// Actual queue status.
 	status blqueue.Status
 
-	once  sync.Once
+	once sync.Once
+
+	// Internal timer. Triggers flush operation according Config.FlushInterval.
 	timer *timer
-	mux   sync.Mutex
-	buf   []byte
+
+	mux sync.Mutex
+	buf []byte
 
 	Err error
 }
 
+// NewQueue makes new dumping queue instance and initialize it according config params.
 func NewQueue(config *Config) (*Queue, error) {
 	q := &Queue{
 		config: config.Copy(),
@@ -34,6 +40,7 @@ func NewQueue(config *Config) (*Queue, error) {
 	return q, q.Err
 }
 
+// Enqueue puts x to the queue.
 func (q *Queue) Enqueue(x interface{}) (err error) {
 	q.once.Do(q.init)
 	if status := q.getStatus(); status == blqueue.StatusClose || status == blqueue.StatusFail {
@@ -43,16 +50,20 @@ func (q *Queue) Enqueue(x interface{}) (err error) {
 	q.mux.Lock()
 	defer q.mux.Unlock()
 
+	// Encode item to bytes.
 	q.buf, err = q.config.Encoder.Encode(q.buf[:0], x)
 	if err != nil {
 		return
 	}
 
+	// Start timer on first incoming item.
+	// Timer will trigger flush operation after Config.FlushInterval since current time.
 	if !q.CheckBit(flagTimer) {
 		q.SetBit(flagTimer, true)
 		go q.timer.wait(q)
 	}
 
+	// Forward encoded item to writer.
 	if _, err = q.config.Writer.Write(q.config.Version, q.buf); err != nil {
 		q.config.MetricsWriter.Fail(q.config.Key, "write fail")
 		return
@@ -61,7 +72,9 @@ func (q *Queue) Enqueue(x interface{}) (err error) {
 
 	q.buf = q.buf[:0]
 
+	// Check if Config.Capacity reached.
 	if q.config.Writer.Size() >= q.config.Capacity {
+		// Reset timer and flush with corresponding reason.
 		q.timer.reset()
 		err = q.flushLF(flushReasonSize)
 	}
@@ -69,6 +82,7 @@ func (q *Queue) Enqueue(x interface{}) (err error) {
 	return
 }
 
+// Size returns actual size in bytes of all queued items (since start or last flush).
 func (q *Queue) Size() int {
 	if q.config.Writer == nil {
 		return 0
@@ -76,14 +90,17 @@ func (q *Queue) Size() int {
 	return int(q.config.Writer.Size())
 }
 
+// Capacity returns maximum queue capacity.
 func (q *Queue) Capacity() int {
 	return int(q.config.Capacity)
 }
 
+// Rate returns size to capacity ratio.
 func (q *Queue) Rate() float32 {
 	return 0
 }
 
+// Close gracefully stops the queue.
 func (q *Queue) Close() error {
 	if q.getStatus() == blqueue.StatusClose {
 		return blqueue.ErrQueueClosed
@@ -100,9 +117,11 @@ func (q *Queue) Close() error {
 	return q.flushLF(flushReasonForce)
 }
 
+// Init the queue.
 func (q *Queue) init() {
 	c := q.config
 
+	// Check mandatory params.
 	if len(c.Key) == 0 {
 		q.Err = blqueue.ErrNoKey
 		q.setStatus(blqueue.StatusFail)
@@ -123,22 +142,28 @@ func (q *Queue) init() {
 		q.setStatus(blqueue.StatusFail)
 		return
 	}
+
+	// Check non-mandatory params and set default values if needed.
 	if c.FlushInterval == 0 {
 		c.FlushInterval = defaultFlushInterval
 	}
 	q.timer = newTimer()
 
 	if c.MetricsWriter == nil {
+		// Use dummy MW.
 		c.MetricsWriter = DummyMetrics{}
 	}
 
+	// Queue is ready!
 	q.setStatus(blqueue.StatusActive)
 }
 
+// Set status of the queue.
 func (q *Queue) setStatus(status blqueue.Status) {
 	atomic.StoreUint32((*uint32)(&q.status), uint32(status))
 }
 
+// Get status of the queue.
 func (q *Queue) getStatus() blqueue.Status {
 	return blqueue.Status(atomic.LoadUint32((*uint32)(&q.status)))
 }
