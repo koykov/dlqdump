6adfd2bd9bc2e7a6d556fe06b09b100d658305a5
Konstantin Voykov <koykov0@gmail.com>
2022-10-10 18:17:58 +0300

    Comment issues: encoder and decoder interface.

--- endec.go
+++ endec.go
@@ -1,9 +1,15 @@
 package dlqdump
 
+// Encoder is the interface that wraps the basic Encode method.
+//
+// Encode encodes x to dst. It returns dst and any error encountered.
 type Encoder interface {
 	Encode(dst []byte, x interface{}) ([]byte, error)
 }
 
+// Decoder is the interface that wraps the basic Write Decode.
+//
+// Decode decodes value from p. It returns decoded value and any error encountered.
 type Decoder interface {
 	Decode(p []byte) (interface{}, error)
 }
