14363d7d5cca0ea83f6fe849fa22326a26200b56
Konstantin Voykov <koykov0@gmail.com>
2022-10-10 18:31:25 +0300

    Comment issues: writer.

--- writer.go
+++ writer.go
@@ -1,7 +1,11 @@
 package dlqdump
 
+// Writer is the interface that wraps the basic Write method.
 type Writer interface {
-	Write(Version, []byte) (int, error)
+	// Write returns the number of bytes written from p (0 <= n <= len(p) + 8) and any error encountered.
+	Write(ver Version, p []byte) (int, error)
+	// Size returns size in bytes of collected data.
 	Size() MemorySize
+	// Flush flushes collected data.
 	Flush() error
 }
