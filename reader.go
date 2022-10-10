e27cd710fb887c7a56e443309c5d36423d952162
Konstantin Voykov <koykov0@gmail.com>
2022-10-10 18:26:13 +0300

    Comment issues: reader.

--- reader.go
+++ reader.go
@@ -1,5 +1,8 @@
 package dlqdump
 
+// Reader is the interface that wraps the basic Read method.
+//
+// Read reads next encoded entry from the dump to dst. It returns version, dst contains entry and any error encountered.
 type Reader interface {
-	Read([]byte) (Version, []byte, error)
+	Read(dst []byte) (Version, []byte, error)
 }
